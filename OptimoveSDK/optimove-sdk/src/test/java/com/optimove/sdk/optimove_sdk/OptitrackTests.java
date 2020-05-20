1ccf4c43a3bbf4df5d9d268cbd01c242011a22a6
Konstantin Antipochkin <>
2020-05-19 20:00:32 -0400

    Added a unit test that simulates a thread chaos

--- OptimoveSDK/optimove-sdk/src/test/java/com/optimove/sdk/optimove_sdk/OptitrackTests.java
+++ OptimoveSDK/optimove-sdk/src/test/java/com/optimove/sdk/optimove_sdk/OptitrackTests.java
@@ -10,6 +10,7 @@ import com.optimove.sdk.optimove_sdk.main.tools.networking.HttpClient;
 import com.optimove.sdk.optimove_sdk.optitrack.OptistreamDbHelper;
 import com.optimove.sdk.optimove_sdk.optitrack.OptistreamEvent;
 import com.optimove.sdk.optimove_sdk.optitrack.OptistreamHandler;
+import com.optimove.sdk.optimove_sdk.optitrack.OptistreamPersistanceAdapter;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -19,11 +20,17 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.internal.verification.api.VerificationData;
+import org.mockito.invocation.Invocation;
+import org.mockito.verification.VerificationMode;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.doAnswer;
@@ -60,33 +67,112 @@ public class OptitrackTests {
     }
 
     @Test
+    public void nonRealtimeEventsShouldBeStoredInOrder() throws Exception {
+
+        int numOfEvents = 100;
+        OptistreamPersistanceAdapter optistreamPersistanceAdapter = new MockedOptistreamPersistency();
+
+        OptistreamHandler optistreamHandler = new OptistreamHandler(httpClient, lifecycleObserver,
+                optistreamPersistanceAdapter, optitrackConfigs);
+
+        for (int i = 0; i < numOfEvents; i++) {
+            optistreamHandler.reportEvents(Collections.singletonList(getRegularEvent(false, "some_name_" + i)));
+        }
+
+        Thread.sleep(1000);
+
+        OptistreamPersistanceAdapter.EventsBulk eventsBulk = optistreamPersistanceAdapter.getFirstEvents(numOfEvents);
+
+        List<String> dbEventJsons = eventsBulk.getEventJsons();
+        Assert.assertEquals(dbEventJsons.size(), numOfEvents);
+
+    }
+
+    @Test
+    public void eventsShouldBeDispatchedOnceAndInOrderInSpiteOfAThreadChaos() throws Exception {
+
+        long maxResponseTime = 80;
+        int numOfEvents = 100;
+        OptistreamPersistanceAdapter optistreamPersistanceAdapter = new MockedOptistreamPersistency();
+
+        OptistreamHandler optistreamHandler = new OptistreamHandler(httpClient, lifecycleObserver,
+                optistreamPersistanceAdapter, optitrackConfigs);
+        applyHttpRandomDelaySuccessInvocation(maxResponseTime);
+
+        //generating numOfEvents events with random realtime
+        for (int i = 0; i < numOfEvents; i++) {
+            optistreamHandler.reportEvents(Collections.singletonList(getRegularEvent(Math.random() > 0.5,
+                    "some_name_" + i)));
+        }
+
+        //add more chaos by simulating the app going on background
+        new Thread(() -> {
+            try {
+                Thread.sleep((long) (Math.random() * maxResponseTime));
+                optistreamHandler.activityStopped();
+            } catch (InterruptedException i) {
+
+            }
+        }).run();
+
+        //The dispatch should be serial, it cant take than the maximum response time numOfEvents times
+        Thread.sleep(maxResponseTime * numOfEvents);
+
+        //verifying that all of the events exist and all of them are ordered
+        verify(httpClient, new VerificationMode() {
+            @Override
+            public void verify(VerificationData data) {
+                int i = 0;
+                for (Invocation invocation : data.getAllInvocations()) {
+                    for (int jsonObjectIndex = 0; jsonObjectIndex < ((JSONArray)invocation.getRawArguments()[1]).length(); jsonObjectIndex++) {
+                        try {
+                            Assert.assertEquals(((JSONArray) invocation.getRawArguments()[1]).getJSONObject(jsonObjectIndex)
+                                    .getString("event"), "some_name_" + i);
+                        } catch (Exception e) {
+                            fail();
+                        }
+                        i++;
+                    }
+                }
+            }
+
+            @Override
+            public VerificationMode description(String description) {
+                return null;
+            }
+        }).postJsonArray(any(), any());
+    }
+
+    @Test
     public void eventShouldBePersisted() {
-        OptistreamEvent regularEvent = getRegularEvent(true);
+        OptistreamEvent regularEvent = getRegularEvent(true, "some_name");
         String regularEventJson = new Gson().toJson(regularEvent);
         OptistreamHandler optistreamHandler = new OptistreamHandler(httpClient, lifecycleObserver, optistreamDbHelper
                 , optitrackConfigs);
         optistreamHandler.reportEvents(Collections.singletonList(regularEvent));
         verify(optistreamDbHelper, timeout(1000)).insertEvent(regularEventJson);
     }
+
     @Test
     public void eventsShouldBeRemovedWhenDispatchSucceed() {
         Gson gson = new Gson();
         String lastId = "some_id";
         OptistreamDbHelper.EventsBulk eventBulk = new OptistreamDbHelper.EventsBulk(lastId,
-               Collections.singletonList(gson.toJson(getRegularEvent(false))));
+                Collections.singletonList(gson.toJson(getRegularEvent(false, "some_name"))));
         applyHttpSuccessInvocation();
         when(optistreamDbHelper.getFirstEvents(OptistreamHandler.Constants.EVENT_BATCH_LIMIT)).thenReturn(eventBulk,
                 new OptistreamDbHelper.EventsBulk(null, new ArrayList<>()));
 
         OptistreamHandler optistreamHandler = new OptistreamHandler(httpClient, lifecycleObserver, optistreamDbHelper
                 , optitrackConfigs);
-        optistreamHandler.reportEvents(Collections.singletonList(getRegularEvent(true)));
+        optistreamHandler.reportEvents(Collections.singletonList(getRegularEvent(true, "some_name")));
 
         verify(optistreamDbHelper, timeout(1000)).removeEvents(lastId);
     }
+
     @Test
     public void realtimeEventShouldBeDispatchedImmediately() throws Exception {
-        OptistreamEvent regularEvent = getRegularEvent(true);
+        OptistreamEvent regularEvent = getRegularEvent(true, "some_name");
         String regularEventJson = new Gson().toJson(regularEvent);
         OptistreamHandler optistreamHandler = new OptistreamHandler(httpClient, lifecycleObserver, optistreamDbHelper
                 , optitrackConfigs);
@@ -104,6 +190,7 @@ public class OptitrackTests {
         Assert.assertEquals(jsonArray.getJSONObject(0)
                 .getString("event"), "some_name");
     }
+
     @Test
     public void notificationEventShouldBeDispatchedImmediately() throws Exception {
         OptistreamEvent notificationEvent = getNotificationEvent();
@@ -124,9 +211,10 @@ public class OptitrackTests {
         Assert.assertEquals(jsonArray.getJSONObject(0)
                 .getString("event"), ScheduledNotificationDeliveredEvent.NAME);
     }
+
     @Test
     public void nonRealtimeEventShouldntBeDispatchedImmediately() throws Exception {
-        OptistreamEvent regularEvent = getRegularEvent(false);
+        OptistreamEvent regularEvent = getRegularEvent(false, "some_name");
         String regularEventJson = new Gson().toJson(regularEvent);
         OptistreamHandler optistreamHandler = new OptistreamHandler(httpClient, lifecycleObserver, optistreamDbHelper
                 , optitrackConfigs);
@@ -140,14 +228,14 @@ public class OptitrackTests {
         verifyZeroInteractions(httpClient);
     }
 
-    private OptistreamEvent getRegularEvent(boolean isRealtime) {
+    private OptistreamEvent getRegularEvent(boolean isRealtime, String name) {
         OptistreamEvent.Metadata optistreamMetadata = mock(OptistreamEvent.Metadata.class);
 
         when(optistreamMetadata.isRealtime()).thenReturn(isRealtime);
         return OptistreamEvent.builder()
                 .withTenantId(33333)
                 .withCategory("some_category")
-                .withName("some_name")
+                .withName(name)
                 .withOrigin("some_origin")
                 .withUserId(userInfo.getUserId())
                 .withVisitorId(userInfo.getVisitorId())
@@ -156,6 +244,7 @@ public class OptitrackTests {
                 .withMetadata(optistreamMetadata)
                 .build();
     }
+
     private OptistreamEvent getNotificationEvent() {
         OptistreamEvent.Metadata optistreamMetadata = mock(OptistreamEvent.Metadata.class);
 
@@ -186,5 +275,86 @@ public class OptitrackTests {
             return builder;
         }).when(builder)
                 .successListener(any());
+    }
+
+    private void applyHttpRandomDelaySuccessInvocation(long maximumDelay) {
+        doAnswer(invocation -> {
+            new Thread(() -> {
+                try {
+                    Thread.sleep((long) (Math.random() * maximumDelay));
+                } catch (InterruptedException in) {
+
+                }
+                Response.Listener<JSONObject> successListener =
+                        (Response.Listener<JSONObject>) invocation.getArguments()[0];
+                JSONObject jsonObject = new JSONObject();
+
+                successListener.onResponse(jsonObject);
+            }).start();
+
+            return builder;
+        }).when(builder)
+                .successListener(any());
+    }
+
+    private class MockedOptistreamPersistency implements OptistreamPersistanceAdapter {
+
+        List<OptistreamEventEntry> optistreamEventsEntries = new ArrayList<>();
+        int currentLastId = 1;
+
+        @Override
+        public boolean insertEvent(String eventJson) {
+            optistreamEventsEntries.add(new OptistreamEventEntry(currentLastId, eventJson));
+            currentLastId++;
+            return true;
+        }
+
+        @Override
+        public void removeEvents(String lastId) {
+            // lastId is an index in this case
+            for (int i = 0; i < Integer.valueOf(lastId); i++) {
+                optistreamEventsEntries.remove(0);
+            }
+        }
+
+        @Override
+        public OptistreamPersistanceAdapter.EventsBulk getFirstEvents(int numberOfEvents) {
+            List<String> optistreamEvents = new ArrayList<>();
+            String lastId = "";
+            for (int i = 0; i < numberOfEvents; i++) {
+                if (i < optistreamEventsEntries.size()) {
+                    optistreamEvents.add(optistreamEventsEntries.get(i)
+                            .getOptistreamEventData());
+                    lastId = String.valueOf(optistreamEventsEntries.get(i).id);
+                }
+            }
+            return new OptistreamPersistanceAdapter.EventsBulk(lastId, optistreamEvents);
+        }
+    }
+
+    private class OptistreamEventEntry {
+        private int id;
+        private String optistreamEventData;
+
+        public OptistreamEventEntry(int id, String optistreamEventData) {
+            this.id = id;
+            this.optistreamEventData = optistreamEventData;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getOptistreamEventData() {
+            return optistreamEventData;
+        }
+
+        public void setOptistreamEventData(String optistreamEventData) {
+            this.optistreamEventData = optistreamEventData;
+        }
     }
 }
