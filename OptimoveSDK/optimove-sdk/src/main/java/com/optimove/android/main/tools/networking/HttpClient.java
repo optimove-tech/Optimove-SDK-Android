2d7823d56a0e09fb3128d118612d5180de832d1a
Konstantin Antipochkin <konstantin_a@optimove.com>
2022-05-25 22:22:50 -0400

    Changed HttpClient to work with OkHttp

--- OptimoveSDK/optimove-sdk/src/main/java/com/optimove/android/main/tools/networking/HttpClient.java
+++ OptimoveSDK/optimove-sdk/src/main/java/com/optimove/android/main/tools/networking/HttpClient.java
@@ -1,67 +1,51 @@
 package com.optimove.android.main.tools.networking;
 
-import android.content.Context;
-
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import com.android.volley.DefaultRetryPolicy;
-import com.android.volley.NetworkResponse;
-import com.android.volley.ParseError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.toolbox.HttpHeaderParser;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.JsonRequest;
-import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.io.IOException;
 
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class HttpClient {
 
     private static final Object lock = new Object();
     private static HttpClient instance;
 
-    public static HttpClient getInstance(Context context) {
+    public static HttpClient getInstance() {
         if (instance != null) {
             return instance;
         }
         synchronized (lock) {
             if (instance == null) {
-                instance = new HttpClient(context);
+                instance = new HttpClient();
             }
         }
         return instance;
     }
 
-    private RequestQueue mainRequestQueue;
-
-    private HttpClient(Context context) {
-        mainRequestQueue = Volley.newRequestQueue(context.getApplicationContext());
-    }
-
-
-    public RequestBuilder<JSONObject> postJson(String baseUrl, JSONObject data) {
-        return new JsonRequestBuilder(baseUrl, data, Request.Method.POST);
-    }
-    public RequestBuilder<JSONObject> postJsonArray(String baseUrl, JSONArray data) {
-        return new JsonArrayRequestBuilder(baseUrl, data, Request.Method.POST);
-    }
-
-    public RequestBuilder<JSONObject> postJsonWithoutJsonResponse(String baseUrl, JSONObject data) {
-        return new JsonRequestBuilderWOJsonResponse(baseUrl, data, Request.Method.POST);
-    }
-    public <T> RequestBuilder<T> postObject(String baseUrl, Class<T> objectType) {
-        return new CustomRequestBuilder<>(baseUrl, objectType, Request.Method.POST);
-    }
+//    public RequestBuilder<JSONObject> postJson(String baseUrl, JSONObject data) {
+//        return new JsonRequestBuilder(baseUrl, data, Request.Method.POST);
+//    }
+//    public RequestBuilder<JSONObject> postJsonArray(String baseUrl, JSONArray data) {
+//        return new JsonArrayRequestBuilder(baseUrl, data, Request.Method.POST);
+//    }
+//
+//    public RequestBuilder<JSONObject> postJsonWithoutJsonResponse(String baseUrl, JSONObject data) {
+//        return new JsonRequestBuilderWOJsonResponse(baseUrl, data, Request.Method.POST);
+//    }
+//    public <T> RequestBuilder<T> postObject(String baseUrl, Class<T> objectType) {
+//        return new CustomRequestBuilder<>(baseUrl, objectType, Request.Method.POST);
+//    }
 
     public <T> RequestBuilder<T> getObject(String baseUrl, Class<T> objectType) {
-        return new CustomRequestBuilder<>(baseUrl, objectType, Request.Method.GET);
+        return new CustomRequestBuilder<>(baseUrl, objectType, 3);
     }
 
     public abstract class RequestBuilder<T> {
@@ -70,9 +54,9 @@ public class HttpClient {
         protected int method;
         protected String url;
         @Nullable
-        protected Response.Listener<T> successListener;
+        protected SuccessListener<T> successListener;
         @Nullable
-        protected Response.ErrorListener errorListener;
+        protected ErrorListener errorListener;
 
         protected RequestBuilder(String baseUrl, int method) {
             this.baseUrl = baseUrl;
@@ -91,12 +75,12 @@ public class HttpClient {
             return this;
         }
 
-        public RequestBuilder<T> successListener(Response.Listener<T> successListener) {
+        public RequestBuilder<T> successListener(SuccessListener<T> successListener) {
             this.successListener = successListener;
             return this;
         }
 
-        public RequestBuilder<T> errorListener(Response.ErrorListener errorListener) {
+        public RequestBuilder<T> errorListener(ErrorListener errorListener) {
             this.errorListener = errorListener;
             return this;
         }
@@ -104,75 +88,75 @@ public class HttpClient {
         public abstract void send();
     }
 
-    public class JsonRequestBuilder extends RequestBuilder<JSONObject> {
-
-        protected JSONObject data;
-
-        protected JsonRequestBuilder(String baseUrl, JSONObject data, int method) {
-            super(baseUrl, method);
-            this.data = data;
-        }
-
-        @Override
-        public void send() {
-            if (url == null) {
-                url = baseUrl;
-            }
-            JsonObjectRequest request = new JsonObjectRequest(method, url, data, successListener, errorListener);
-            request.setRetryPolicy(new DefaultRetryPolicy(60000, 2, 2));
-            mainRequestQueue.add(request);
-        }
-    }
-    public class JsonArrayRequestBuilder extends RequestBuilder<JSONObject> {
-
-        protected JSONArray data;
-
-        protected JsonArrayRequestBuilder(String baseUrl, JSONArray data, int method) {
-            super(baseUrl, method);
-            this.data = data;
-        }
-
-        @Override
-        public void send() {
-            if (url == null) {
-                url = baseUrl;
-            }
-            CustomJsonArrayRequest request = new CustomJsonArrayRequest(method, url, data, successListener, errorListener);
-            request.setRetryPolicy(new DefaultRetryPolicy(60000, 2, 2));
-            mainRequestQueue.add(request);
-        }
-    }
-    public class JsonRequestBuilderWOJsonResponse extends RequestBuilder<JSONObject> {
-
-        protected JSONObject data;
-
-        protected JsonRequestBuilderWOJsonResponse(String baseUrl, JSONObject data, int method) {
-            super(baseUrl, method);
-            this.data = data;
-        }
-        @Override
-        public void send() {
-            if (url == null) {
-                url = baseUrl;
-            }
-            JsonObjectRequest request = new JsonObjectRequest(method, url, data, successListener, errorListener) {
-                @Override
-                protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
-
-                    String json = new String(response.data, StandardCharsets.UTF_8);
-
-                    if (json.length() == 0) {
-                        return Response.success(null, HttpHeaderParser.parseCacheHeaders(response));
-                    } else {
-                        return super.parseNetworkResponse(response);
-                    }
-                }
-            };
-            request.setRetryPolicy(new DefaultRetryPolicy(60000, 2, 2));
-            mainRequestQueue.add(request);
-        }
-    }
-
+//    public class JsonRequestBuilder extends RequestBuilder<JSONObject> {
+//
+//        protected JSONObject data;
+//
+//        protected JsonRequestBuilder(String baseUrl, JSONObject data, int method) {
+//            super(baseUrl, method);
+//            this.data = data;
+//        }
+//
+//        @Override
+//        public void send() {
+//            if (url == null) {
+//                url = baseUrl;
+//            }
+//            JsonObjectRequest request = new JsonObjectRequest(method, url, data, successListener, errorListener);
+//            request.setRetryPolicy(new DefaultRetryPolicy(60000, 2, 2));
+//            mainRequestQueue.add(request);
+//        }
+//    }
+//    public class JsonArrayRequestBuilder extends RequestBuilder<JSONObject> {
+//
+//        protected JSONArray data;
+//
+//        protected JsonArrayRequestBuilder(String baseUrl, JSONArray data, int method) {
+//            super(baseUrl, method);
+//            this.data = data;
+//        }
+//
+//        @Override
+//        public void send() {
+//            if (url == null) {
+//                url = baseUrl;
+//            }
+//            CustomJsonArrayRequest request = new CustomJsonArrayRequest(method, url, data, successListener, errorListener);
+//            request.setRetryPolicy(new DefaultRetryPolicy(60000, 2, 2));
+//            mainRequestQueue.add(request);
+//        }
+//    }
+//    public class JsonRequestBuilderWOJsonResponse extends RequestBuilder<JSONObject> {
+//
+//        protected JSONObject data;
+//
+//        protected JsonRequestBuilderWOJsonResponse(String baseUrl, JSONObject data, int method) {
+//            super(baseUrl, method);
+//            this.data = data;
+//        }
+//        @Override
+//        public void send() {
+//            if (url == null) {
+//                url = baseUrl;
+//            }
+//            JsonObjectRequest request = new JsonObjectRequest(method, url, data, successListener, errorListener) {
+//                @Override
+//                protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
+//
+//                    String json = new String(response.data, StandardCharsets.UTF_8);
+//
+//                    if (json.length() == 0) {
+//                        return Response.success(null, HttpHeaderParser.parseCacheHeaders(response));
+//                    } else {
+//                        return super.parseNetworkResponse(response);
+//                    }
+//                }
+//            };
+//            request.setRetryPolicy(new DefaultRetryPolicy(60000, 2, 2));
+//            mainRequestQueue.add(request);
+//        }
+//    }
+//
     public class CustomRequestBuilder<T> extends RequestBuilder<T> {
 
         Class<T> typeToParse;
@@ -188,42 +172,66 @@ public class HttpClient {
                 url = baseUrl;
             }
 
-            CustomRequest<T> customRequest = new CustomRequest<>(method, url, typeToParse, successListener,
-                    errorListener);
-            customRequest.setRetryPolicy(new DefaultRetryPolicy(60000, 2, 2));
-            mainRequestQueue.add(customRequest);
+            Request request = new Request.Builder().url(url).get().build();
+
+            new OkHttpClient().newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                   //trigger failure
+                }
+
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                    // trigger success listener
+                    if (!response.isSuccessful() || response.body() == null) {
+                        //fail
+                        return;
+                    }
+                    //success with
+                    successListener.sendResponse(new Gson().fromJson(response.body().string(), typeToParse));
+                }
+            });
         }
 
     }
-    public class CustomJsonArrayRequest extends JsonRequest<JSONObject> {
 
-        /**
-         * Creates a new request.
-         * @param method the HTTP method to use
-         * @param url URL to fetch the JSON from
-         * @param jsonRequest A {@link JSONObject} to post with the request. Null is allowed and
-         *   indicates no parameters will be posted along with request.
-         * @param listener Listener to receive the JSON response
-         * @param errorListener Error listener, or null to ignore errors.
-         */
-        public CustomJsonArrayRequest(int method, String url, JSONArray jsonRequest,
-                                      Response.Listener<JSONObject> listener, Response.ErrorListener errorListener) {
-            super(method, url, (jsonRequest == null) ? null : jsonRequest.toString(), listener,
-                    errorListener);
-        }
-
-        @Override
-        protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
-            try {
-                String jsonString = new String(response.data,
-                        HttpHeaderParser.parseCharset(response.headers, PROTOCOL_CHARSET));
-                return Response.success(new JSONObject(jsonString),
-                        HttpHeaderParser.parseCacheHeaders(response));
-            } catch (UnsupportedEncodingException e) {
-                return Response.error(new ParseError(e));
-            } catch (JSONException je) {
-                return Response.error(new ParseError(je));
-            }
-        }
+    public interface SuccessListener<T> {
+        void sendResponse(T response);
     }
+
+    public interface ErrorListener {
+        void sendError(Throwable throwable);
+    }
+
+//    public class CustomJsonArrayRequest extends JsonRequest<JSONObject> {
+//
+//        /**
+//         * Creates a new request.
+//         * @param method the HTTP method to use
+//         * @param url URL to fetch the JSON from
+//         * @param jsonRequest A {@link JSONObject} to post with the request. Null is allowed and
+//         *   indicates no parameters will be posted along with request.
+//         * @param listener Listener to receive the JSON response
+//         * @param errorListener Error listener, or null to ignore errors.
+//         */
+//        public CustomJsonArrayRequest(int method, String url, JSONArray jsonRequest,
+//                                      Response.Listener<JSONObject> listener, Response.ErrorListener errorListener) {
+//            super(method, url, (jsonRequest == null) ? null : jsonRequest.toString(), listener,
+//                    errorListener);
+//        }
+//
+//        @Override
+//        protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
+//            try {
+//                String jsonString = new String(response.data,
+//                        HttpHeaderParser.parseCharset(response.headers, PROTOCOL_CHARSET));
+//                return Response.success(new JSONObject(jsonString),
+//                        HttpHeaderParser.parseCacheHeaders(response));
+//            } catch (UnsupportedEncodingException e) {
+//                return Response.error(new ParseError(e));
+//            } catch (JSONException je) {
+//                return Response.error(new ParseError(je));
+//            }
+//        }
+//    }
 }
