53beedee3595829da9b7caa89a191792ebcbce84
Tom Barzilay <tom_b@optimove.com>
2022-11-21 17:43:53 +0200

    called handler.cancel() explicitly in onReceivedSslError()

--- OptimoveSDK/optimove-sdk/src/main/java/com/optimove/android/optimobile/InAppMessageView.java
+++ OptimoveSDK/optimove-sdk/src/main/java/com/optimove/android/optimobile/InAppMessageView.java
@@ -348,7 +348,7 @@ class InAppMessageView extends WebViewClient {
     @Override
     public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
         super.onReceivedSslError(view, handler, error);
-
+        handler.cancel();
         closeDialog(currentActivity);
     }
 
