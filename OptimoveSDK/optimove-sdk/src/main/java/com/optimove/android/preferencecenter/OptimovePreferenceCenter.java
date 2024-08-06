9bba0898e4b685c45ef5a17c13409e9955f730f4
Vladislav Voicehovich <vladislav_v@optimove.com>
2024-08-06 15:48:29 +0100

    handle all foreseeble channels

--- OptimoveSDK/optimove-sdk/src/main/java/com/optimove/android/preferencecenter/OptimovePreferenceCenter.java
+++ OptimoveSDK/optimove-sdk/src/main/java/com/optimove/android/preferencecenter/OptimovePreferenceCenter.java
@@ -49,7 +49,7 @@ public class OptimovePreferenceCenter {
     }
 
     public enum Channel {
-        MOBILE_PUSH(489), WEB_PUSH(490), SMS(493);
+        MOBILE_PUSH(489), WEB_PUSH(490), SMS(493), IN_APP(427), WHATSAPP(498), MAIL(15), INBOX(495);
         private final int channel;
 
         Channel(int channel) {
@@ -69,6 +69,14 @@ public class OptimovePreferenceCenter {
                     return WEB_PUSH;
                 case 493:
                     return SMS;
+                case 427:
+                    return IN_APP;
+                case 498:
+                    return WHATSAPP;
+                case 15:
+                    return MAIL;
+                case 495: 
+                    return INBOX;
                 default:
                     throw new IllegalArgumentException("Preference center does not support channel " + value);
             }
